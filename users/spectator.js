6262952085ef525047efe71181a644bb56d5ef20
Trufi <stevemyz@gmail.com>
2014-08-21 12:38:07 +0700

    delete old

--- users/spectator.js
+++
@@ -1,8 +0,0 @@
-var game = require('/game');
-
-function Spectator(options) {
-    this.id = options.id;
-    this.socket = options.socket;
-    this.sid = options.sid;
-    this.game = game.spectatorGame();
-}
